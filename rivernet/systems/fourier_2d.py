5edd1d0822e6a9d61adfc8270f8e1bb4db1b34fd
Alasdair Tran <alasdair.tran@gmail.com>
2021-04-15 14:02:44 +1000

    Log prediction heat maps

--- rivernet/systems/fourier_2d.py
+++ rivernet/systems/fourier_2d.py
@@ -1,4 +1,6 @@
+import matplotlib.pyplot as plt
 import torch
+import wandb
 from einops import repeat
 from rivernet.modules import Module
 from rivernet.modules.loss import LpLoss
@@ -25,6 +27,8 @@ class Fourier2DSystem(System):
     def _learning_step(self, batch):
         xx, yy = batch
         B, X, Y, _ = xx.shape
+        # Note here the in_channels consists of the first 10 time steps
+        # and 2 positional encodings.
         # xx.shape == [batch_size, x_dim, y_dim, in_channels]
         # yy.shape == [batch_size, x_dim, y_dim, out_channels]
 
@@ -33,6 +37,7 @@ class Fourier2DSystem(System):
         grid_y = repeat(ticks, 'y -> b x y 1', b=B, x=X)
 
         loss = 0
+        # We predict one future one step at a time
         for t in range(self.n_steps):
             y = yy[..., t: t+1]
             # y.shape == [batch_size, x_dim, y_dim, 1]
@@ -45,21 +50,28 @@ class Fourier2DSystem(System):
         loss /= self.n_steps
         loss_full = self.l2_loss(pred.reshape(B, -1), yy.reshape(B, -1))
 
-        return loss, loss_full
+        return loss, loss_full, pred
 
     def training_step(self, batch, batch_idx):
-        loss, loss_full = self._learning_step(batch)
+        loss, loss_full, _ = self._learning_step(batch)
         self.log('train_loss', loss)
         self.log('train_loss_full', loss_full)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        loss, loss_full = self._learning_step(batch)
+        loss, loss_full, preds = self._learning_step(batch)
         self.log('valid_loss', loss)
         self.log('valid_loss_full', loss_full)
 
+        if batch_idx == 0:
+            xx, yy = batch
+            expt = self.logger.experiment
+            log_imshow(expt, xx[0, :, :, -1], 'gt t=9')
+            log_imshow(expt, yy[0, :, :, -1], 'gt t=19')
+            log_imshow(expt, preds[0, :, :, -1], 'pred t=19')
+
     def test_step(self, batch, batch_idx):
-        loss, loss_full = self._learning_step(batch)
+        loss, loss_full, _ = self._learning_step(batch)
         self.log('test_loss', loss)
         self.log('test_loss_full', loss_full)
 
@@ -68,3 +80,13 @@ class Fourier2DSystem(System):
         scheduler = torch.optim.lr_scheduler.StepLR(
             opt, step_size=100, gamma=0.5)
         return [opt], [scheduler]
+
+
+def log_imshow(expt, tensor, name):
+    fig = plt.figure(figsize=(6, 6))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.imshow(tensor.cpu().numpy(),
+              interpolation='bilinear', cmap=plt.get_cmap('bwr'))
+    fig.tight_layout()
+    expt.log({f'{name}': wandb.Image(fig)})
+    plt.close('all')
