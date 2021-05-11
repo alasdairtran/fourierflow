a6db33c975dacb594dc88c59c1171ebe14ee83f3
Alasdair Tran <alasdair.tran@gmail.com>
2021-05-11 07:04:17 +0000

    Copy X-transformer code

--- fourierflow/modules/__init__.py
+++ fourierflow/modules/__init__.py
@@ -1,3 +1,4 @@
+from .autoregressive_wrapper import AutoregressiveWrapper
 from .decoders import MLPDecoder, ODEDecoder
 from .encoders import Encoder, MuSigmaEncoder
 from .fourier_2d import SimpleBlock2d
@@ -9,3 +10,4 @@ from .processes import NeuralODEProcess, NeuralProcess
 from .radflow import LSTMDecoder
 from .radflow_attention import AttentionDecoder
 from .schedulers import LinearWithWarmupScheduler
+from .x_transformers import XTransformer
