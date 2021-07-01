0283a78f6dbb95692a8328d01a0fd7db0a3d7296
Alasdair Tran <alasdair.tran@gmail.com>
2021-07-01 12:14:04 +0000

    Fix up Navier-Stokes generation code

--- fourierflow/datastores/synthetic/ns_2d.py
+++ fourierflow/datastores/synthetic/ns_2d.py
@@ -2,6 +2,7 @@ import math
 from timeit import default_timer
 
 import h5py
+import numpy as np
 import torch
 from tqdm import tqdm
 
@@ -28,11 +29,9 @@ def navier_stokes_2d(w0, f, visc, T, delta_t=1e-4, record_steps=1):
     steps = math.ceil(T/delta_t)
 
     # Initial vorticity to Fourier space
-#     w_h = torch.rfft(w0, 2, normalized=False, onesided=False)
     w_h = torch.fft.fftn(w0, dim=[1, 2], norm='backward')
 
     # Forcing to Fourier space
-#     f_h = torch.rfft(f, 2, normalized=False, onesided=False)
     f_h = torch.fft.fftn(f, dim=[0, 1], norm='backward')
 
     # If same forcing for the whole batch
@@ -42,17 +41,22 @@ def navier_stokes_2d(w0, f, visc, T, delta_t=1e-4, record_steps=1):
     # Record solution every this number of steps
     record_time = math.floor(steps/record_steps)
 
-    #Wavenumbers in y-direction
-    k_y = torch.cat((torch.arange(start=0, end=k_max, step=1, device=w0.device), torch.arange(
-        start=-k_max, end=0, step=1, device=w0.device)), 0).repeat(N, 1)
-    #Wavenumbers in x-direction
+    # Wavenumbers in y-direction
+    k_y = torch.cat((
+        torch.arange(start=0, end=k_max, step=1, device=w0.device),
+        torch.arange(start=-k_max, end=0, step=1, device=w0.device)),
+        0).repeat(N, 1)
+    # Wavenumbers in x-direction
     k_x = k_y.transpose(0, 1)
     # Negative Laplacian in Fourier space
-    lap = 4*(math.pi**2)*(k_x**2 + k_y**2)
+    lap = 4 * (math.pi**2) * (k_x**2 + k_y**2)
     lap[0, 0] = 1.0
     # Dealiasing mask
-    dealias = torch.unsqueeze(torch.logical_and(torch.abs(k_y) <= (
-        2.0/3.0)*k_max, torch.abs(k_x) <= (2.0/3.0)*k_max).float(), 0)
+    dealias = torch.unsqueeze(
+        torch.logical_and(
+            torch.abs(k_y) <= (2.0 / 3.0) * k_max,
+            torch.abs(k_x) <= (2.0 / 3.0)*k_max
+        ).float(), 0)
 
     # Saving solution and time
     sol = torch.zeros(*w0.size(), record_steps, device=w0.device)
@@ -64,67 +68,52 @@ def navier_stokes_2d(w0, f, visc, T, delta_t=1e-4, record_steps=1):
     t = 0.0
     for j in tqdm(range(steps)):
         # Stream function in Fourier space: solve Poisson equation
-        psi_h = w_h.clone()
-        psi_h.real = psi_h.real/lap
-        psi_h.imag = psi_h.imag/lap
+        psi_h = w_h / lap
 
         # Velocity field in x-direction = psi_y
         q = psi_h.clone()
-        temp = q.real.clone()
-        q.real = -2*math.pi*k_y*q.imag
-        q.imag = 2*math.pi*k_y*temp
-#         q = torch.irfft(q, 2, normalized=False,
-#                         onesided=False, signal_sizes=(N, N))
+        q_real_temp = q.real.clone()
+        q.real = -2 * math.pi * k_y * q.imag
+        q.imag = 2 * math.pi * k_y * q_real_temp
         q = torch.fft.ifftn(q, dim=[1, 2], norm='backward').real
 
         # Velocity field in y-direction = -psi_x
         v = psi_h.clone()
-        temp = v.real.clone()
-        v.real = 2*math.pi*k_x*v.imag
-        v.imag = -2*math.pi*k_x*temp
-#         v = torch.irfft(v, 2, normalized=False,
-#                         onesided=False, signal_sizes=(N, N))
+        v_real_temp = v.real.clone()
+        v.real = 2 * math.pi * k_x * v.imag
+        v.imag = -2 * math.pi * k_x * v_real_temp
         v = torch.fft.ifftn(v, dim=[1, 2], norm='backward').real
 
         # Partial x of vorticity
         w_x = w_h.clone()
-        temp = w_x.real.clone()
-        w_x.real = -2*math.pi*k_x*w_x.imag
-        w_x.imag = 2*math.pi*k_x*temp
-#         w_x = torch.irfft(w_x, 2, normalized=False,
-#                           onesided=False, signal_sizes=(N, N))
+        w_x_temp = w_x.real.clone()
+        w_x.real = -2 * math.pi * k_x * w_x.imag
+        w_x.imag = 2 * math.pi * k_x * w_x_temp
         w_x = torch.fft.ifftn(w_x, dim=[1, 2], norm='backward').real
 
         # Partial y of vorticity
         w_y = w_h.clone()
-        temp = w_y.real.clone()
-        w_y.real = -2*math.pi*k_y*w_y.imag
-        w_y.imag = 2*math.pi*k_y*temp
-#         w_y = torch.irfft(w_y, 2, normalized=False,
-#                           onesided=False, signal_sizes=(N, N))
+        w_y_temp = w_y.real.clone()
+        w_y.real = -2 * math.pi * k_y * w_y.imag
+        w_y.imag = 2 * math.pi * k_y * w_y_temp
         w_y = torch.fft.ifftn(w_y, dim=[1, 2], norm='backward').real
 
         # Non-linear term (u.grad(w)): compute in physical space then back to Fourier space
-#         F_h = torch.rfft(q*w_x + v*w_y, 2, normalized=False, onesided=False)
-        F_h = torch.fft.fftn(q*w_x + v*w_y, dim=[1, 2], norm='backward')
+        F_h = torch.fft.fftn(q * w_x + v * w_y, dim=[1, 2], norm='backward')
 
         # Dealias
-        F_h.real = dealias * F_h.real
-        F_h.imag = dealias * F_h.imag
+        F_h *= dealias
 
         # Cranck-Nicholson update
-        w_h.real = (-delta_t*F_h.real + delta_t*f_h.real + (1.0 -
-                                                            0.5*delta_t*visc*lap)*w_h.real)/(1.0 + 0.5*delta_t*visc*lap)
-        w_h.imag = (-delta_t*F_h[..., 1] + delta_t*f_h.imag + (1.0 -
-                                                               0.5*delta_t*visc*lap)*w_h.imag)/(1.0 + 0.5*delta_t*visc*lap)
+        factor = 0.5 * delta_t * visc * lap
+        num = -delta_t * F_h + delta_t * f_h + (1.0 - factor) * w_h
+        w_h = num / (1.0 + factor)
 
         # Update real time (used only for recording)
         t += delta_t
 
-        if (j+1) % record_time == 0:
+        if (j + 1) % record_time == 0:
             # Solution in physical space
-            #             w = torch.irfft(w_h, 2, normalized=False,
-            #                             onesided=False, signal_sizes=(N, N))
             w = torch.fft.ifftn(w_h, dim=[1, 2], norm='backward').real
 
             # Record solution and time
@@ -153,7 +142,7 @@ t = torch.linspace(0, 1, s+1, device=device)
 t = t[0:-1]
 
 X, Y = torch.meshgrid(t, t)
-f = 0.1*(torch.sin(2*math.pi*(X + Y)) + torch.cos(2*math.pi*(X + Y)))
+f = 0.1*(torch.sin(2 * math.pi * (X + Y)) + torch.cos(2 * math.pi * (X + Y)))
 
 # Number of snapshots from solution
 record_steps = 50
@@ -182,7 +171,9 @@ for j in range(N//bsize):
     w0 = GRF.sample(bsize)
 
     # Solve NS
-    sol, sol_t = navier_stokes_2d(w0, f, viscosity, 50.0, 1e-4, record_steps)
+    with torch.no_grad():
+        sol, sol_t = navier_stokes_2d(w0, f, viscosity,
+                                      50.0, 1e-4, record_steps)
 
     data_f['a'][c:(c+bsize), ...] = w0.cpu().numpy()
     data_f['u'][c:(c+bsize), ...] = sol.cpu().numpy()
