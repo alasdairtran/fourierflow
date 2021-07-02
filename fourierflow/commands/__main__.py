47c35d9d1af2d7ca2aeb727df97df8ff5b2695f1
dan <dan@danmackinlay.name>
2021-07-02 16:56:50 +1000

    move Zongyi data download to command

--- fourierflow/commands/__main__.py
+++ fourierflow/commands/__main__.py
@@ -9,6 +9,7 @@ import typer
 import wandb
 from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
+import gdown
 
 from fourierflow.common import Datastore, Experiment
 from fourierflow.utils.parsing import yaml_to_params
@@ -91,6 +92,51 @@ def test(config_path: str,
     trainer = pl.Trainer(logger=wandb_logger,
                          **params.pop('trainer').as_dict())
     trainer.test(experiment, datamodule=datastore)
+
+
+
+@app.command()
+def download_zongyi(config_path: str,
+         checkpoint_path: str,
+         overrides: str = '',
+         map_location: Optional[str] = None,
+         debug: bool = False):
+    """Download some google datasets. 
+
+    Should probably be in a separate module.
+
+    Copied from a shell script:
+
+    mkdir data/fourier && cd data/fourier
+    gdown --id 16a8od4vidbiNR3WtaBPCSZ0T3moxjhYe # Burgers_R10.zip
+    gdown --id 1nzT0-Tu-LS2SoMUCcmO1qyjQd6WC9OdJ # Burgers_v100.zip
+    gdown --id 1G9IW_2shmfgprPYISYt_YS8xa87p4atu # Burgers_v1000.zip
+    gdown --id 1ViDqN7nc_VCnMackiXv_d7CHZANAFKzV # Darcy_241.zip
+    gdown --id 1Z1uxG9R8AdAGJprG5STcphysjm56_0Jf # Darcy_421.zip
+    gdown --id 1r3idxpsHa21ijhlu3QQ1hVuXcqnBTO7d # NavierStokes_V1e-3_N5000_T50.zip
+    gdown --id 1pr_Up54tNADCGhF8WLvmyTfKlCD5eEkI # NavierStokes_V1e-4_N20_T50_R256_test.zip
+    gdown --id 1RmDQQ-lNdAceLXrTGY_5ErvtINIXnpl3 # NavierStokes_V1e-4_N10000_T30.zip
+    gdown --id 1lVgpWMjv9Z6LEv3eZQ_Qgj54lYeqnGl5 # NavierStokes_V1e-5_N1200_T20.zip
+    unzip *.zip && rm -rf *.zip
+    """
+    params = yaml_to_params(config_path, overrides)
+
+    print(params)
+    for hashid, fname in [
+        ("16a8od4vidbiNR3WtaBPCSZ0T3moxjhYe", "Burgers_R10.zip"),
+        ("1nzT0-Tu-LS2SoMUCcmO1qyjQd6WC9OdJ", "Burgers_v100.zip"),
+        ("1G9IW_2shmfgprPYISYt_YS8xa87p4atu", "Burgers_v1000.zip"),
+        ("1ViDqN7nc_VCnMackiXv_d7CHZANAFKzV", "Darcy_241.zip"),
+        ("1Z1uxG9R8AdAGJprG5STcphysjm56_0Jf", "Darcy_421.zip"),
+        ("1r3idxpsHa21ijhlu3QQ1hVuXcqnBTO7d", "NavierStokes_V1e-3_N5000_T50.zip"),
+        ("1pr_Up54tNADCGhF8WLvmyTfKlCD5eEkI", "NavierStokes_V1e-4_N20_T50_R256_test.zip"),
+        ("1RmDQQ-lNdAceLXrTGY_5ErvtINIXnpl3", "NavierStokes_V1e-4_N10000_T30.zip"),
+        ("1lVgpWMjv9Z6LEv3eZQ_Qgj54lYeqnGl5", "NavierStokes_V1e-5_N1200_T20.zip"),
+    ]:
+        gdown.download(
+            "https://drive.google.com/uc?id={hashid}".format(hashid=hashid),
+            fname, postprocess=gdown.extractall)
+
 
 
 if __name__ == "__main__":
