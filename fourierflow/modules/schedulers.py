6a7b74e67351cc70a4502e842ccb25eeb9a6daa7
Alasdair Tran <alasdair.tran@gmail.com>
2021-07-22 04:54:27 +0000

    Avoid nested functions to allow pickling

--- fourierflow/modules/schedulers.py
+++ fourierflow/modules/schedulers.py
@@ -5,18 +5,39 @@ from torch.optim.lr_scheduler import LambdaLR
 from fourierflow.common import Scheduler
 
 
+class LinearLRLambda:
+    def __init__(self, num_warmup_steps, num_training_steps):
+        self.num_warmup_steps = num_warmup_steps
+        self.num_training_steps = num_training_steps
+
+    def __call__(self, current_step):  # the function formerly known as "bar"
+        if current_step < self.num_warmup_steps:
+            return float(current_step) / float(max(1, self.num_warmup_steps))
+        return max(0.0, float(self.num_training_steps - current_step) /
+                   float(max(1, self.num_training_steps - self.num_warmup_steps)))
+
+
 @Scheduler.register('linear_with_warmup')
 class LinearWithWarmupScheduler(Scheduler, LambdaLR):
     def __init__(self, optimizer, num_warmup_steps: int,
                  num_training_steps: int, last_epoch=-1, verbose=False):
 
-        def lr_lambda(current_step: int):
-            if current_step < num_warmup_steps:
-                return float(current_step) / float(max(1, num_warmup_steps))
-            return max(0.0, float(num_training_steps - current_step) /
-                       float(max(1, num_training_steps - num_warmup_steps)))
-
+        lr_lambda = LinearLRLambda(num_warmup_steps, num_training_steps)
         super().__init__(optimizer, lr_lambda, last_epoch, verbose)
+
+
+class CosineLRLambda:
+    def __init__(self, num_warmup_steps, num_training_steps, num_cycles):
+        self.num_warmup_steps = num_warmup_steps
+        self.num_training_steps = num_training_steps
+        self.num_cycles = num_cycles
+
+    def __call__(self, current_step):  # the function formerly known as "bar"
+        if current_step < self.num_warmup_steps:
+            return float(current_step) / float(max(1, self.num_warmup_steps))
+        progress = float(current_step - self.num_warmup_steps) / \
+            float(max(1, self.num_training_steps - self.num_warmup_steps))
+        return max(0.0, 0.5 * (1.0 + math.cos(math.pi * float(self.num_cycles) * 2.0 * progress)))
 
 
 @Scheduler.register('cosine_with_warmup')
@@ -24,12 +45,6 @@ class CosineWithWarmupScheduler(Scheduler, LambdaLR):
     def __init__(self, optimizer, num_warmup_steps: int,
                  num_training_steps: int, num_cycles: float = 0.5,
                  last_epoch=-1, verbose=False):
-
-        def lr_lambda(current_step):
-            if current_step < num_warmup_steps:
-                return float(current_step) / float(max(1, num_warmup_steps))
-            progress = float(current_step - num_warmup_steps) / \
-                float(max(1, num_training_steps - num_warmup_steps))
-            return max(0.0, 0.5 * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress)))
-
+        lr_lambda = CosineLRLambda(
+            num_warmup_steps, num_training_steps, num_cycles)
         super().__init__(optimizer, lr_lambda, last_epoch, verbose)
