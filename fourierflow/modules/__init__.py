7cdfdcf9f09206c3e22dedd31b16141da403a469
Alasdair Tran <alasdair.tran@gmail.com>
2021-07-27 15:19:46 +0000

    Treat each image dimension separately

--- fourierflow/modules/__init__.py
+++ fourierflow/modules/__init__.py
@@ -2,6 +2,7 @@ from .autoregressive_wrapper import AutoregressiveWrapper
 from .decoders import MLPDecoder, ODEDecoder
 from .encoders import Encoder, MuSigmaEncoder
 from .fourier_2d import SimpleBlock2d
+from .fourier_2d_factorized import SimpleBlock2dFactorized
 from .fourier_2d_int import SimpleBlock2dIntegrate
 from .fourier_2d_shared import SimpleBlock2dShared
 from .fourier_2d_split import SimpleBlock2dSplit
