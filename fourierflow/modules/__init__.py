c7f9385456ffa3e2c2a5c6a9424ada6ba22081c6
Alasdair Tran <alasdair.tran@gmail.com>
2021-07-28 23:51:19 +0000

    Add fourier dimensions independently

--- fourierflow/modules/__init__.py
+++ fourierflow/modules/__init__.py
@@ -3,6 +3,7 @@ from .decoders import MLPDecoder, ODEDecoder
 from .encoders import Encoder, MuSigmaEncoder
 from .fourier_2d import SimpleBlock2d
 from .fourier_2d_factorized import SimpleBlock2dFactorized
+from .fourier_2d_factorized_parallel import SimpleBlock2dFactorizedParallel
 from .fourier_2d_int import SimpleBlock2dIntegrate
 from .fourier_2d_shared import SimpleBlock2dShared
 from .fourier_2d_split import SimpleBlock2dSplit
