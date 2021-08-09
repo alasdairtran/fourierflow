ab60988f4b37c8a076d4d30a1f8d0fec671db1cf
Alasdair Tran <alasdair.tran@gmail.com>
2021-08-09 06:30:37 +0000

    Implement ExponentialWithWarmupScheduler

--- fourierflow/modules/schedulers.py
+++ fourierflow/modules/schedulers.py
@@ -48,3 +48,24 @@ class CosineWithWarmupScheduler(Scheduler, LambdaLR):
         lr_lambda = CosineLRLambda(
             num_warmup_steps, num_training_steps, num_cycles)
         super().__init__(optimizer, lr_lambda, last_epoch, verbose)
+
+
+class ExponentialLRLambda:
+    def __init__(self, num_warmup_steps, gamma):
+        self.num_warmup_steps = num_warmup_steps
+        self.gamma = gamma
+
+    def __call__(self, current_step):
+        if current_step < self.num_warmup_steps:
+            return float(current_step) / float(max(1, self.num_warmup_steps))
+
+        progress = current_step - self.num_warmup_steps
+        return self.gamma**progress
+
+
+@Scheduler.register('exponential_with_warmup')
+class ExponentialWithWarmupScheduler(Scheduler, LambdaLR):
+    def __init__(self, optimizer, num_warmup_steps: int,
+                 gamma=0.9999, last_epoch=-1, verbose=False):
+        lr_lambda = ExponentialLRLambda(num_warmup_steps, gamma)
+        super().__init__(optimizer, lr_lambda, last_epoch, verbose)
