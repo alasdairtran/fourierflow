67a6b70156da3ab885664fa9185efc6b406661d4
Alasdair Tran <alasdair.tran@gmail.com>
2021-08-10 14:58:44 +0000

    Make deepcopy compatible with weight norm

--- fourierflow/modules/fourier_2d_factorized_parallel.py
+++ fourierflow/modules/fourier_2d_factorized_parallel.py
@@ -6,6 +6,7 @@ to propagates in time.
 """
 
 
+import copy
 import math
 from functools import partial
 
@@ -14,12 +15,50 @@ import torch.nn as nn
 import torch.nn.functional as F
 from einops import rearrange
 from torch.nn.utils import weight_norm
+from torch.nn.utils.weight_norm import WeightNorm
 
 from fourierflow.common import Module
 
 
-def wnorm(module, active):
-    return weight_norm(module) if active else module
+class WNLinear(nn.Linear):
+    def __init__(self, in_features: int, out_features: int, bias: bool = True, device=None, dtype=None, wnorm=False):
+        super().__init__(in_features=in_features,
+                         out_features=out_features,
+                         bias=bias,
+                         device=device,
+                         dtype=dtype)
+        if wnorm:
+            weight_norm(self)
+
+        self._fix_weight_norm_deepcopy()
+
+    def _fix_weight_norm_deepcopy(self):
+        # Fix bug where deepcopy doesn't work with weightnorm.
+        # Taken from https://github.com/pytorch/pytorch/issues/28594#issuecomment-679534348
+        orig_deepcopy = getattr(self, '__deepcopy__', None)
+
+        def __deepcopy__(self, memo):
+            # save and delete all weightnorm weights on self
+            weights = {}
+            for hook in self._forward_pre_hooks.values():
+                if isinstance(hook, WeightNorm):
+                    weights[hook.name] = getattr(self, hook.name)
+                    delattr(self, hook.name)
+            # remove this deepcopy method, restoring the object's original one if necessary
+            __deepcopy__ = self.__deepcopy__
+            if orig_deepcopy:
+                self.__deepcopy__ = orig_deepcopy
+            else:
+                del self.__deepcopy__
+            # actually do the copy
+            result = copy.deepcopy(self)
+            # restore weights and method on self
+            for name, value in weights.items():
+                setattr(self, name, value)
+            self.__deepcopy__ = __deepcopy__
+            return result
+        # bind __deepcopy__ to the weightnorm'd layer
+        self.__deepcopy__ = __deepcopy__.__get__(self, self.__class__)
 
 
 class FeedForward(nn.Module):
@@ -30,7 +69,7 @@ class FeedForward(nn.Module):
             in_dim = dim if i == 0 else dim * factor
             out_dim = dim if i == n_layers - 1 else dim * factor
             self.layers.append(nn.Sequential(
-                wnorm(nn.Linear(in_dim, out_dim), ff_weight_norm),
+                WNLinear(in_dim, out_dim, wnorm=ff_weight_norm),
                 nn.Dropout(dropout),
                 nn.ReLU(inplace=True) if i < n_layers - 1 else nn.Identity(),
                 nn.LayerNorm(out_dim) if layer_norm and i == n_layers -
@@ -153,7 +192,7 @@ class SimpleBlock2dFactorizedParallel(nn.Module):
 
         self.modes = modes
         self.width = width
-        self.in_proj = wnorm(nn.Linear(input_dim, self.width), ff_weight_norm)
+        self.in_proj = WNLinear(input_dim, self.width, wnorm=ff_weight_norm)
         self.drop = nn.Dropout(in_dropout)
         self.next_input = next_input
         self.avg_outs = avg_outs
@@ -195,8 +234,8 @@ class SimpleBlock2dFactorizedParallel(nn.Module):
                                                        mode=mode))
 
         self.out = nn.Sequential(
-            wnorm(nn.Linear(self.width, 128), ff_weight_norm),
-            wnorm(nn.Linear(128, 1), ff_weight_norm))
+            WNLinear(self.width, 128, wnorm=ff_weight_norm),
+            WNLinear(128, 1, wnorm=ff_weight_norm))
 
     def forward(self, x):
         # x.shape == [n_batches, *dim_sizes, input_size]
