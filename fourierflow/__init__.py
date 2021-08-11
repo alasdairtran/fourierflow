d1cfbb2291e357e85dde755daf16f057c0c82f21
Alasdair Tran <alasdair.tran@gmail.com>
2021-08-11 08:10:33 +0000

    Import callbacks and schedulers

--- fourierflow/__init__.py
+++ fourierflow/__init__.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv  # isort:skip
 load_dotenv()  # noqa
 
+import fourierflow.callbacks
 import fourierflow.datastores
 import fourierflow.experiments
 import fourierflow.modules
+import fourierflow.schedulers
