6babdb119ac09c3ce9160b2548f15f4e9a1b8dd3
Alasdair Tran <alasdair.tran@gmail.com>
2021-08-15 01:29:48 +0000

    Separate out train, valid, and test splits

--- fourierflow/commands/generate.py
+++ fourierflow/commands/generate.py
@@ -46,7 +46,9 @@ def get_random_force(b, s, device, cycles, scaling):
 @app.command()
 def navier_stokes(
     path: str = Argument(..., help='Path to store the generated samples'),
-    n: int = Option(1400, help='Number of solutions to generate'),
+    n_train: int = Option(1000, help='Number of train solutions to generate'),
+    n_valid: int = Option(200, help='Number of valid solutions to generate'),
+    n_test: int = Option(200, help='Number of test solutions to generate'),
     s: int = Option(256, help='Width of the solution grid'),
     t: int = Option(20, help='Final time step'),
     steps: int = Option(20, help='Number of snapshots from solution'),
@@ -55,7 +57,7 @@ def navier_stokes(
     mu_max: float = Option(1e-5, help='Maximum viscoity'),
     seed: int = Option(23893, help='Seed value for reproducibility'),
     delta: float = Option(1e-4, help='Internal time step for sovler'),
-    b: int = Option(100, help='Batch size'),
+    batch_size: int = Option(100, help='Batch size'),
     force: Force = Option(Force.li, help='Type of forcing function'),
     cycles: int = Option(2, help='Number of cycles in forcing function'),
     scaling: float = Option(0.1, help='Scaling of forcing function'),
@@ -84,35 +86,42 @@ def navier_stokes(
         f = 0.1*(torch.sin(2 * math.pi * (X + Y)) +
                  torch.cos(2 * math.pi * (X + Y)))
 
-    c = 0
     data_f = h5py.File(path, 'a')
-    data_f.create_dataset('a', (n, s, s), np.float32)
-    data_f.create_dataset('f', (n, s, s), np.float32)
-    data_f.create_dataset('u', (n, s, s, steps), np.float32)
-    data_f.create_dataset('mu', (n,), np.float32)
 
-    b = min(n, b)
-    with torch.no_grad():
-        for j in range(n // b):
-            print('batch', j)
-            w0 = GRF.sample(b)
+    def generate_split(n, split):
+        print('Generating split:', split)
+        data_f.create_dataset(f'{split}/a', (n, s, s), np.float32)
+        data_f.create_dataset(f'{split}/f', (n, s, s), np.float32)
+        data_f.create_dataset(f'{split}/u', (n, s, s, steps), np.float32)
+        data_f.create_dataset(f'{split}/mu', (n,), np.float32)
+        b = min(n, batch_size)
+        c = 0
 
-            if force == Force.random:
-                f = get_random_force(b, s, device, cycles, scaling)
+        with torch.no_grad():
+            for j in range(n // b):
+                print('batch', j)
+                w0 = GRF.sample(b)
 
-            if mu_min != mu_max:
-                mu = np.random.rand(b) * (mu_max - mu_min) + mu_min
+                if force == Force.random:
+                    f = get_random_force(b, s, device, cycles, scaling)
 
-            sol, _ = solve_navier_stokes_2d(w0, f, mu, t, delta, steps)
-            data_f['a'][c:(c+b), ...] = w0.cpu().numpy()
-            data_f['u'][c:(c+b), ...] = sol
+                if mu_min != mu_max:
+                    mu = np.random.rand(b) * (mu_max - mu_min) + mu_min
 
-            if force == Force.random:
-                data_f['f'][c:(c+b), ...] = f.cpu().numpy()
+                sol, _ = solve_navier_stokes_2d(w0, f, mu, t, delta, steps)
+                data_f[f'{split}/a'][c:(c+b), ...] = w0.cpu().numpy()
+                data_f[f'{split}/u'][c:(c+b), ...] = sol
 
-            data_f['mu'][c:(c+b)] = mu
+                if force == Force.random:
+                    data_f[f'{split}/f'][c:(c+b), ...] = f.cpu().numpy()
 
-            c += b
+                data_f[f'{split}/mu'][c:(c+b)] = mu
+
+                c += b
+
+    generate_split(n_train, 'train')
+    generate_split(n_valid, 'valid')
+    generate_split(n_test, 'test')
 
 
 if __name__ == "__main__":
