4d787613d68f2c1ed871d61609ad3ccf27554737
Alasdair Tran <alasdair.tran@gmail.com>
2021-08-19 08:53:36 +0000

    Tidy up training entrypoint with helper functions

--- fourierflow/commands/train.py
+++ fourierflow/commands/train.py
@@ -17,6 +17,37 @@ from fourierflow.utils.parsing import yaml_to_params
 app = Typer()
 
 
+def get_save_dir(config_path):
+    # Determine the path where the experimental results will be saved.
+    parts = config_path.split('/')
+    i = parts.index('experiments')
+    root_dir = os.path.expandvars('$SM_MODEL_DIR')
+    save_dir = os.path.join(root_dir, *parts[i+1:-1])
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir, exist_ok=True)
+    return save_dir
+
+
+def delete_old_results(results_dir, force):
+    """Delete existing checkpoints and wandb logs if --force is enabled."""
+    wandb_dir = os.path.join(results_dir, 'wandb')
+    chkpt_dir = os.path.join(results_dir, 'checkpoints')
+    if force and os.path.exists(wandb_dir):
+        shutil.rmtree(wandb_dir)
+    if force and os.path.exists(chkpt_dir):
+        shutil.rmtree(chkpt_dir)
+
+
+def upload_code_to_wandb(config_path, wandb_logger):
+    """Upload all Python code for save the exact state of the experiment."""
+    code_artifact = wandb.Artifact('fourierflow', type='code')
+    code_artifact.add_file(config_path, 'config.yaml')
+    paths = glob('fourierflow/**/*.py')
+    for path in paths:
+        code_artifact.add_file(path, path)
+    wandb_logger.experiment.log_artifact(code_artifact)
+
+
 @app.callback(invoke_without_command=True)
 def main(config_path: str, overrides: str = '', force: bool = False, debug: bool = False):
     """Train a Pytorch Lightning experiment."""
@@ -26,46 +57,27 @@ def main(config_path: str, overrides: str = '', force: bool = False, debug: bool
     if debug:
         ptvsd.enable_attach(address=('0.0.0.0', 5678))
         ptvsd.wait_for_attach()
+        # ptvsd doesn't play well with multiple processes.
+        params['datastore']['n_workers'] = 0
 
-    # Determine the path where the experimental results will be saved.
-    parts = config_path.split('/')
-    i = parts.index('experiments')
-    save_dir = os.path.expandvars('$SM_MODEL_DIR')
-    results_dir = os.path.join(save_dir, *parts[i+1:-1])
-    if not os.path.exists(results_dir):
-        os.makedirs(results_dir, exist_ok=True)
-
-    # Delete existing checkpoints and wandb logs if --force is enabled.
-    wandb_dir = os.path.join(results_dir, 'wandb')
-    chkpt_dir = os.path.join(results_dir, 'checkpoints')
-    if force and os.path.exists(wandb_dir):
-        shutil.rmtree(wandb_dir)
-    if force and os.path.exists(chkpt_dir):
-        shutil.rmtree(chkpt_dir)
+    # Set up directories to save experimental outputs.
+    save_dir = get_save_dir(config_path)
+    delete_old_results(save_dir, force)
 
     # We use Weights & Biases to track our experiments.
     wandb_opts = params.pop('wandb').as_dict()
-    wandb_logger = WandbLogger(save_dir=results_dir,
+    wandb_logger = WandbLogger(save_dir=save_dir,
                                mode=os.environ.get('WANDB_MODE', 'online'),
                                config=deepcopy(params.as_dict()),
                                id=str(uuid.uuid4()),
                                **wandb_opts)
 
-    # To ensure reproduciblity, we seed the whole Pytorch Lightning pipeline.
+    # Set seed and upload code for reproducibility.
     seed = params.get('seed', 38124)
     pl.seed_everything(seed, workers=True)
+    upload_code_to_wandb(config_path, wandb_logger)
 
-    # Upload all Python code for save the exact state of the experiment code.
-    code_artifact = wandb.Artifact('fourierflow', type='code')
-    code_artifact.add_file(config_path, 'config.yaml')
-    paths = glob('fourierflow/**/*.py')
-    for path in paths:
-        code_artifact.add_file(path, path)
-    wandb_logger.experiment.log_artifact(code_artifact)
-
-    # ptvsd doesn't play well with multiple processes.
-    if debug:
-        params['datastore']['n_workers'] = 0
+    # Initialize the dataset and experiment modules.
     datastore = Datastore.from_params(params['datastore'])
     experiment = Experiment.from_params(params['experiment'])
 
@@ -81,7 +93,7 @@ def main(config_path: str, overrides: str = '', force: bool = False, debug: bool
     trainer = pl.Trainer(logger=wandb_logger,
                          callbacks=callbacks,
                          plugins=plugins,
-                         weights_save_path=results_dir,
+                         weights_save_path=save_dir,
                          **params.pop('trainer').as_dict())
 
     # Tuning only has an effect when either auto_scale_batch_size or
