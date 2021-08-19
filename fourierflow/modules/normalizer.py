86f21c781c9435c83d4e352bf9126a8730a05c24
Alasdair Tran <alasdair.tran@gmail.com>
2021-08-19 09:14:22 +0000

    Refactor dimension pooling in normalizer

--- fourierflow/modules/normalizer.py
+++ fourierflow/modules/normalizer.py
@@ -13,6 +13,7 @@ class Normalizer(nn.Module):
         self.register_buffer('sum_squared', torch.full(size, 0.0))
         self.register_buffer('one', torch.tensor(1.0))
         self.register_buffer('std_epsilon', torch.full(size, std_epsilon))
+        self.dim_sizes = None
 
     def _accumulate(self, x):
         x_count = x.shape[0]
@@ -24,27 +25,47 @@ class Normalizer(nn.Module):
         self.count += x_count
         self.n_accumulations += 1
 
-    def forward(self, x):
-        _, *dim_sizes, _ = x.shape
-        if dim_sizes:
+    def _pool_dims(self, x):
+        if self.dim_sizes is None:
+            _, *dim_sizes, _ = x.shape
+            self.dim_sizes = dim_sizes
+
+        if self.dim_sizes:
             x = rearrange(x, 'b ... h -> (b ...) h')
+
+        return x
+
+    def _unpool_dims(self, x):
+        if len(self.dim_sizes) == 1:
+            x = rearrange(x, '(b m) h -> b m h', m=self.dim_sizes[0])
+        elif len(self.dim_sizes) == 2:
+            m, n = self.dim_sizes
+            x = rearrange(x, '(b m n) h -> b m n h', m=m, n=n)
+        return x
+
+    def forward(self, x):
+        x = self._pool_dims(x)
         # x.shape == [batch_size, latent_dim]
 
         if self.training and self.n_accumulations < self.max_accumulations:
             self._accumulate(x)
 
         x = (x - self.mean) / self.std
-
-        if len(dim_sizes) == 1:
-            x = rearrange(x, '(b m) h -> b m h', m=dim_sizes[0])
-        elif len(dim_sizes) == 2:
-            m, n = dim_sizes
-            x = rearrange(x, '(b m n) h -> b m n h', m=m, n=n)
+        x = self._unpool_dims(x)
 
         return x
 
-    def inverse(self, x):
-        return x * self.std + self.mean
+    def inverse(self, x, channel=None):
+        x = self._pool_dims(x)
+
+        if channel is None:
+            x = x * self.std + self.mean
+        else:
+            x = x * self.std[channel] + self.mean[channel]
+
+        x = self._unpool_dims(x)
+
+        return x
 
     @property
     def mean(self):
