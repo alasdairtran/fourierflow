9e4de48c67c4265618b0622b887c94d811715d64
Alasdair Tran <alasdair.tran@gmail.com>
2021-08-23 14:22:40 +0000

    Implement MeshCFD

--- fourierflow/experiments/__init__.py
+++ fourierflow/experiments/__init__.py
@@ -8,6 +8,7 @@ from .fourier_2d import Fourier2DExperiment
 from .fourier_2d_darcy import Fourier2DDarcyExperiment
 from .fourier_2d_scales import Fourier2DScalesExperiment
 from .fourier_2d_single import Fourier2DSingleExperiment
+from .mesh_cfd import MeshCFD
 from .nbeats import NBEATSExperiment
 from .nodep import TimeSeriesODE
 from .radflow import RadflowExperiment
