53f91263cd7ca955fb6e899dafeca36e956cf620
Alasdair Tran <alasdair.tran@gmail.com>
2021-08-24 09:11:01 +0000

    Move WNLinear to separate submodule

--- fourierflow/modules/fourier_2d_factorized_parallel.py
+++ fourierflow/modules/fourier_2d_factorized_parallel.py
@@ -1,64 +1,11 @@
-"""
-@author: Zongyi Li This file is the Fourier Neural Operator for 2D problem such
-as the Navier-Stokes equation discussed in Section 5.3 in the
-[paper](https://arxiv.org/pdf/2010.08895.pdf), which uses a recurrent structure
-to propagates in time.
-"""
-
-
-import copy
-import math
-from functools import partial
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from einops import rearrange
-from torch.nn.utils import weight_norm
-from torch.nn.utils.weight_norm import WeightNorm
 
 from fourierflow.registries import Module
 
-
-class WNLinear(nn.Linear):
-    def __init__(self, in_features: int, out_features: int, bias: bool = True, device=None, dtype=None, wnorm=False):
-        super().__init__(in_features=in_features,
-                         out_features=out_features,
-                         bias=bias,
-                         device=device,
-                         dtype=dtype)
-        if wnorm:
-            weight_norm(self)
-
-        self._fix_weight_norm_deepcopy()
-
-    def _fix_weight_norm_deepcopy(self):
-        # Fix bug where deepcopy doesn't work with weightnorm.
-        # Taken from https://github.com/pytorch/pytorch/issues/28594#issuecomment-679534348
-        orig_deepcopy = getattr(self, '__deepcopy__', None)
-
-        def __deepcopy__(self, memo):
-            # save and delete all weightnorm weights on self
-            weights = {}
-            for hook in self._forward_pre_hooks.values():
-                if isinstance(hook, WeightNorm):
-                    weights[hook.name] = getattr(self, hook.name)
-                    delattr(self, hook.name)
-            # remove this deepcopy method, restoring the object's original one if necessary
-            __deepcopy__ = self.__deepcopy__
-            if orig_deepcopy:
-                self.__deepcopy__ = orig_deepcopy
-            else:
-                del self.__deepcopy__
-            # actually do the copy
-            result = copy.deepcopy(self)
-            # restore weights and method on self
-            for name, value in weights.items():
-                setattr(self, name, value)
-            self.__deepcopy__ = __deepcopy__
-            return result
-        # bind __deepcopy__ to the weightnorm'd layer
-        self.__deepcopy__ = __deepcopy__.__get__(self, self.__class__)
+from .linear import WNLinear
 
 
 class FeedForward(nn.Module):
--- fourierflow/modules/linear.py
+++ fourierflow/modules/linear.py
@@ -1,7 +1,10 @@
+import copy
 import logging
 import math
 
 import torch.nn as nn
+from torch.nn.utils import weight_norm
+from torch.nn.utils.weight_norm import WeightNorm
 
 logger = logging.getLogger(__name__)
 
@@ -33,3 +36,44 @@ class GehringLinear(nn.Linear):
         # Kingma (2016): https://arxiv.org/abs/1602.07868.
         if self.weight_norm:
             nn.utils.weight_norm(self)
+
+
+class WNLinear(nn.Linear):
+    def __init__(self, in_features: int, out_features: int, bias: bool = True, device=None, dtype=None, wnorm=False):
+        super().__init__(in_features=in_features,
+                         out_features=out_features,
+                         bias=bias,
+                         device=device,
+                         dtype=dtype)
+        if wnorm:
+            weight_norm(self)
+
+        self._fix_weight_norm_deepcopy()
+
+    def _fix_weight_norm_deepcopy(self):
+        # Fix bug where deepcopy doesn't work with weightnorm.
+        # Taken from https://github.com/pytorch/pytorch/issues/28594#issuecomment-679534348
+        orig_deepcopy = getattr(self, '__deepcopy__', None)
+
+        def __deepcopy__(self, memo):
+            # save and delete all weightnorm weights on self
+            weights = {}
+            for hook in self._forward_pre_hooks.values():
+                if isinstance(hook, WeightNorm):
+                    weights[hook.name] = getattr(self, hook.name)
+                    delattr(self, hook.name)
+            # remove this deepcopy method, restoring the object's original one if necessary
+            __deepcopy__ = self.__deepcopy__
+            if orig_deepcopy:
+                self.__deepcopy__ = orig_deepcopy
+            else:
+                del self.__deepcopy__
+            # actually do the copy
+            result = copy.deepcopy(self)
+            # restore weights and method on self
+            for name, value in weights.items():
+                setattr(self, name, value)
+            self.__deepcopy__ = __deepcopy__
+            return result
+        # bind __deepcopy__ to the weightnorm'd layer
+        self.__deepcopy__ = __deepcopy__.__get__(self, self.__class__)
