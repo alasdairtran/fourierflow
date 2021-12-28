c075fa78b3224135a975a2aedf72d56c75880ef6
Alasdair Tran <alasdair.tran@gmail.com>
2021-12-28 14:08:42 +0000

    Support importing dotted module paths

--- fourierflow/utils/__init__.py
+++ fourierflow/utils/__init__.py
@@ -1,4 +1,4 @@
 from .exceptions import ExistingExperimentFound
 from .helpers import cache_fn, default, exists
 from .logger import setup_logger
-from .path import get_experiment_id, get_save_dir
+from .path import get_experiment_id, get_save_dir, import_string
--- fourierflow/utils/path.py
+++ fourierflow/utils/path.py
@@ -1,5 +1,7 @@
 import os
+import sys
 from datetime import datetime
+from importlib import import_module
 from pathlib import Path
 
 
@@ -21,3 +23,27 @@ def get_experiment_id(checkpoint_id, trial, save_dir, resume):
         checkpoint_id = next(paths).parent.name
     now = datetime.now().strftime('%Y%m%d-%H%M%S-%f')
     return checkpoint_id or f'trial-{trial}-{now}'
+
+
+def import_string(dotted_path):
+    """Import a dotted module path.
+
+    And return the attribute/class designated by the
+    last name in the path. Raise ImportError if the import failed.
+
+    Adatped from https://stackoverflow.com/a/34963527/3790116.
+    """
+    try:
+        module_path, class_name = dotted_path.rsplit('.', 1)
+    except ValueError as e:
+        msg = "%s doesn't look like a module path" % dotted_path
+        raise ImportError.with_traceback(ImportError(msg), sys.exc_info()[2])
+
+    module = import_module(module_path)
+
+    try:
+        return getattr(module, class_name)
+    except AttributeError:
+        msg = 'Module "%s" does not define a "%s" attribute/class' % (
+            module_path, class_name)
+        raise ImportError.with_traceback(ImportError(msg), sys.exc_info()[2])
