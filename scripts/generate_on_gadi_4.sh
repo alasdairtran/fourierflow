b211ad457489ad9e437b95d27c8ab1ee445a14b3
Alasdair Tran <alasdair.tran@gmail.com>
2021-12-31 09:45:00 +0000

    Add script to generate on 4 GPUs

--- scripts/generate_on_gadi_4.sh
+++ scripts/generate_on_gadi_4.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#PBS -l ncpus=12
-#PBS -l ngpus=1
+#PBS -l ncpus=48
+#PBS -l ngpus=4
 #PBS -l walltime=48:00:00
-#PBS -l mem=96GB
+#PBS -l mem=384GB
 #PBS -l jobfs=20MB
 #PBS -P v89
 #PBS -q gpuvolta
 #PBS -l other=gdata1
 #PBS -l storage=scratch/v89+gdata/v89
 #PBS -M alasdair.tran@anu.edu.au
-#PBS -N fourierflow-1
+#PBS -N fourierflow-4
 #PBS -j oe
 #PBS -m abe
 #PBS -l wd
@@ -17,4 +17,4 @@
 source $HOME/.bashrc
 cd /g/data/v89/at3219/projects/fourierflow
 
-CUDA_VISIBLE_DEVICES=0 poetry run fourierflow generate $DATA $CONFIG_DIR
+CUDA_VISIBLE_DEVICES=0,1,2,3 poetry run fourierflow generate --devices 0,1,2,3 $DATA $CONFIG_PATH
