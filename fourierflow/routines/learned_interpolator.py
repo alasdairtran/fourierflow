2d2c8b8d657f9c287c1fbdca1e86ab75b9c5dab9
Alasdair Tran <alasdair.tran@gmail.com>
2022-01-04 11:50:17 +0000

    Override init_step and train_step

--- fourierflow/routines/learned_interpolator.py
+++ fourierflow/routines/learned_interpolator.py
@@ -1,11 +1,18 @@
+from typing import Any, Mapping
+
 import elegy as eg
 import haiku as hk
+import jax
 import jax.numpy as jnp
+from elegy.model.model import M
+from elegy.model.model_core import TrainStepOutput
+from elegy.types import Logs
 from jax_cfd.base.boundaries import periodic_boundary_conditions
 from jax_cfd.base.grids import Grid, GridArray, GridVariable
 from jax_cfd.ml.advections import ConvectionModule
 from jax_cfd.ml.equations import modular_navier_stokes_model
 from jax_cfd.ml.physics_specifications import BasePhysicsSpecs
+from treex import LossAndLogs
 
 
 class LearnedInterpolator(eg.Model):
@@ -38,3 +45,53 @@ class LearnedInterpolator(eg.Model):
 
     def __call__(self):
         pass
+
+    def init_step(self: M,
+                  key: jnp.ndarray,
+                  inputs: Any) -> M:
+        model: M = self
+
+        if model.module is not None:
+            model.module = model.module.init(key, inputs=inputs)
+
+        if model.optimizer is not None:
+            params = model.parameters()
+            model.optimizer = model.optimizer.init(params)
+
+        losses, metrics = model._losses_and_metrics.value
+        aux_losses = model.loss_logs()
+        aux_metrics = model.metric_logs()
+
+        model.loss_and_logs = LossAndLogs(
+            losses=losses,
+            metrics=metrics,
+            aux_losses=aux_losses,
+            aux_metrics=aux_metrics,
+        )
+
+        model = model.distributed_strategy.handle_post_init(model)
+
+        return model
+
+    def train_step(self: M,
+                   inputs: Any,
+                   labels: Mapping[str, Any]) -> TrainStepOutput[M]:
+        model: M = self
+        grads: M
+        logs: Logs
+
+        assert model.module is not None
+        params = model.parameters(self._is_trainable)
+
+        grad_fn = jax.grad(self.loss_fn, has_aux=True)
+        grads, (logs, model) = grad_fn(params, model, inputs, labels)
+
+        model, grads = model.distributed_strategy.handle_model_and_grads(
+            model, grads)
+
+        assert model.optimizer is not None
+
+        params = model.optimizer.update(grads, params)
+        model = model.merge(params)
+
+        return logs, model
