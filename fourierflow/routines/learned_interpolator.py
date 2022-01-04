af93ba1bc55c85480058da4ec50e4287396d615c
Alasdair Tran <alasdair.tran@gmail.com>
2022-01-04 12:14:25 +0000

    Override test_step and pred_step

--- fourierflow/routines/learned_interpolator.py
+++ fourierflow/routines/learned_interpolator.py
@@ -5,7 +5,8 @@ import haiku as hk
 import jax
 import jax.numpy as jnp
 from elegy.model.model import M
-from elegy.model.model_core import TrainStepOutput
+from elegy.model.model_core import (PredStepOutput, TestStepOutput,
+                                    TrainStepOutput)
 from elegy.types import Logs
 from jax_cfd.base.boundaries import periodic_boundary_conditions
 from jax_cfd.base.funcutils import init_context
@@ -13,7 +14,7 @@ from jax_cfd.base.grids import Grid, GridArray, GridVariable
 from jax_cfd.ml.advections import ConvectionModule
 from jax_cfd.ml.equations import modular_navier_stokes_model
 from jax_cfd.ml.physics_specifications import BasePhysicsSpecs
-from treex import LossAndLogs
+from treex import Inputs, LossAndLogs
 
 
 class LearnedInterpolator(eg.Model):
@@ -86,3 +87,57 @@ class LearnedInterpolator(eg.Model):
         model = model.merge(params)
 
         return logs, model
+
+    def test_step(self: M, inputs: Any, labels: Mapping[str, Any]) -> TestStepOutput[M]:
+        model: M = self
+
+        preds, model = model.pred_step(inputs)
+        assert model.loss_and_logs is not None
+
+        aux_losses = model.loss_logs()
+        aux_metrics = model.metric_logs()
+
+        extended_labels = {
+            "inputs": inputs,
+            "preds": preds,
+            "model": model,
+            "parameters": model.parameters(),
+            "batch_stats": model.batch_stats(),
+            "rngs": model.rngs(),
+            "model_states": model.model_states(),
+            "states": model.states(),
+            "metric_logs": model.metric_logs(),
+            "loss_logs": model.loss_logs(),
+            "logs": model.logs(),
+            **labels,
+        }
+
+        losses_kwargs = extended_labels
+        metrics_kwargs = extended_labels
+
+        losses_kwargs, metrics_kwargs = model.distributed_strategy.handle_lm_kwargs(
+            losses_kwargs, metrics_kwargs
+        )
+
+        loss, losses_logs, metrics_logs = model.loss_and_logs.batch_loss_epoch_logs(
+            **losses_kwargs,
+            metrics_kwargs=metrics_kwargs,
+            aux_losses=aux_losses,
+            aux_metrics=aux_metrics,
+        )
+
+        losses_logs, metrics_logs = model.distributed_strategy.handle_lm_logs(
+            losses_logs, metrics_logs
+        )
+
+        logs = {**losses_logs, **metrics_logs}
+
+        return loss, logs, model
+
+    def pred_step(self: M, inputs: Any,) -> PredStepOutput[M]:
+        model: M = self
+        inputs_obj = Inputs.from_value(inputs)
+
+        preds = model.module(*inputs_obj.args, **inputs_obj.kwargs)
+
+        return preds, model
