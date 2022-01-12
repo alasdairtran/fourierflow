c010f0ee721d6a24515ccaf97d9d091fd93dba6b
Alasdair Tran <alasdair.tran@gmail.com>
2022-01-12 06:28:48 +0000

    Register import resolver

--- fourierflow/__init__.py
+++ fourierflow/__init__.py
@@ -9,6 +9,8 @@ import fourierflow.callbacks
 import fourierflow.modules
 import fourierflow.routines
 import fourierflow.schedulers
+from fourierflow.utils import import_string
 
 # Allow partial instantiations of optimizers and schedulers.
 OmegaConf.register_new_resolver("get_method", hydra.utils.get_method)
+OmegaConf.register_new_resolver("import", import_string)
