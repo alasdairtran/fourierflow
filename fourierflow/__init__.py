66dba8585913c41924641fd3c4a452b68d022ff4
Alasdair Tran <alasdair.tran@gmail.com>
2022-01-12 06:56:04 +0000

    Register eval resolver

--- fourierflow/__init__.py
+++ fourierflow/__init__.py
@@ -13,4 +13,7 @@ from fourierflow.utils import import_string
 
 # Allow partial instantiations of optimizers and schedulers.
 OmegaConf.register_new_resolver("get_method", hydra.utils.get_method)
+# Allow importing constants
 OmegaConf.register_new_resolver("import", import_string)
+# Remove hack once this is resolved: https://github.com/omry/omegaconf/issues/91
+OmegaConf.register_new_resolver("eval", eval)
