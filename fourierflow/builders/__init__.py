e205a67f9630aa004ff310804560077a980e50fb
Alasdair Tran <alasdair.tran@gmail.com>
2022-01-19 05:27:12 +0000

    Export KolmogorovElegyDataset

--- fourierflow/builders/__init__.py
+++ fourierflow/builders/__init__.py
@@ -1,5 +1,5 @@
-from .kolmogorov import (KolmogorovBuilder, KolmogorovTorchDataset,
-                         generate_kolmogorov)
+from .kolmogorov import (KolmogorovBuilder, KolmogorovElegyDataset,
+                         KolmogorovTorchDataset, generate_kolmogorov)
 from .ns_contextual import NSContextualBuilder
 from .ns_markov import NSMarkovBuilder
 from .ns_zongyi import NSZongyiBuilder
