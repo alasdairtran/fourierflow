14ff86cf8c6dfba0edd52539f5b5bb6f8055d983
Alasdair Tran <alasdair.tran@gmail.com>
2022-01-24 11:57:34 +0000

    Compute correlation

--- fourierflow/routines/learned_interpolator.py
+++ fourierflow/routines/learned_interpolator.py
@@ -11,11 +11,16 @@ from elegy.types import Logs
 from jax_cfd.base.boundaries import periodic_boundary_conditions
 from jax_cfd.base.funcutils import init_context
 from jax_cfd.base.grids import Grid, GridArray, GridVariable
+from jax_cfd.base.resize import downsample_staggered_velocity
 from jax_cfd.ml.advections import ConvectionModule
 from jax_cfd.ml.equations import modular_navier_stokes_model
 from jax_cfd.ml.physics_specifications import BasePhysicsSpecs
 from optax import l2_loss
+from tqdm import tqdm
 from treex import Inputs, LossAndLogs
+
+from fourierflow.utils import velocity_to_vorticity
+from fourierflow.utils.array import downsample_vorticity
 
 
 class LearnedInterpolator(eg.Model):
@@ -25,18 +30,23 @@ class LearnedInterpolator(eg.Model):
                  physics_specs: BasePhysicsSpecs,
                  convection_module: ConvectionModule,
                  **kwargs):
-        grid = Grid(shape=(size, size),
-                    domain=((0, 2 * jnp.pi), (0, 2 * jnp.pi)))
+        sim_grid = Grid(shape=(size, size),
+                        domain=((0, 2 * jnp.pi), (0, 2 * jnp.pi)))
+        self.sim_grid = sim_grid
+
+        out_grid = Grid(shape=(32, 32),
+                        domain=((0, 2 * jnp.pi), (0, 2 * jnp.pi)))
+        self.out_grid = out_grid
 
         def step_fwd(vx, vy, vorticity):
             inputs = []
-            for v, offset in zip([vx, vy], grid.cell_faces):
+            for v, offset in zip([vx, vy], sim_grid.cell_faces):
                 variable = GridVariable(
-                    array=GridArray(v[0], offset, grid),
-                    bc=periodic_boundary_conditions(grid.ndim))
+                    array=GridArray(v[0], offset, sim_grid),
+                    bc=periodic_boundary_conditions(sim_grid.ndim))
                 inputs.append(variable)
             model = modular_navier_stokes_model(
-                grid, dt, physics_specs,
+                sim_grid, dt, physics_specs,
                 convection_module=convection_module)
             return model(inputs)
 
@@ -110,7 +120,8 @@ class LearnedInterpolator(eg.Model):
 
         T = inputs['data'].shape[-1]  # [1, 32, 32, 2441]
 
-        for t in range(T):
+        w_preds = []
+        for t in tqdm(range(T)):
             batch = {
                 'vorticity': inputs['data'][..., t],
                 'vx': inputs['vx'][..., t],
@@ -121,6 +132,23 @@ class LearnedInterpolator(eg.Model):
             vx_loss = l2_loss(vx_pred, batch['vx'][0]).mean()
             vy_loss = l2_loss(vy_pred, batch['vy'][0]).mean()
             loss = vx_loss + vy_loss
+
+            if vx_pred.shape[1] > 32:
+                vx_pred, vy_pred = downsample_staggered_velocity(
+                    self.sim_grid, self.out_grid, preds)
+
+            w_pred = velocity_to_vorticity(vx_pred, vy_pred, self.out_grid)
+            w_preds.append(w_pred)
+
+        w_preds = jnp.stack(w_preds, axis=-1)
+        w_targets = inputs['corr_data'][0][...]
+
+        w_pred_norm = jnp.linalg.norm(w_preds, axis=(0, 1), keepdims=True)
+        w_target_norm = jnp.linalg.norm(
+            w_targets, axis=(0, 1), keepdims=True)
+
+        rho = (w_preds / w_pred_norm) * (w_targets / w_target_norm)
+        rho = rho.sum(axis=(0, 1))
 
         logs = {'loss': loss}
 
