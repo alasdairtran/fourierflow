4ab59046ba79cf49c20afc837aa15bb2b4f56699
Alasdair Tran <alasdair.tran@gmail.com>
2022-02-07 07:42:58 +0000

    Implement callback base class

--- fourierflow/callbacks/__init__.py
+++ fourierflow/callbacks/__init__.py
@@ -1,3 +1,4 @@
+from .callback import Callback
 from .model_checkpoint import CustomModelCheckpoint
 from .stochastic_weight_averaging import StochasticWeightAveraging
 from .wandb_logger import WandbLogger
