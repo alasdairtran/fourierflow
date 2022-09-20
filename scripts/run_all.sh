2ec356479a55d156110b5e68a44243ee3cbce954
Alasdair Tran <alasdair.tran@gmail.com>
2022-09-20 04:01:36 +0000

    Clean up superresolution configs

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -2,12 +2,22 @@
 
 
 # Super-resolution tests
-CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_64/32/config.yaml
-CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_64/64/config.yaml
-CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_64/128/config.yaml
-CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_64/256/config.yaml
+CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_x32_x64/32/config.yaml
+CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_x32_x64/64/config.yaml
+CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_x32_x64/128/config.yaml
+CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_x32_x64/256/config.yaml
 
-CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_64_128/32/config.yaml
-CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_64_128/64/config.yaml
-CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_64_128/128/config.yaml
-CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_64_128/256/config.yaml
+CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_x32_x128/32/config.yaml
+CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_x32_x128/64/config.yaml
+CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_x32_x128/128/config.yaml
+CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_x32_x128/256/config.yaml
+
+CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_x64/32/config.yaml
+CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_x64/64/config.yaml
+CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_x64/128/config.yaml
+CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_x64/256/config.yaml
+
+CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_x64_x128/32/config.yaml
+CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_x64_x128/64/config.yaml
+CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_x64_x128/128/config.yaml
+CUDA_VISIBLE_DEVICES=0 fourierflow test --trial 0 --force experiments/torus_kochkov/ffno/superresolution/train_with_x64_x128/256/config.yaml
