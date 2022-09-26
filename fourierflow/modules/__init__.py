899f64cb657075874ef520f4e9991a4e189d8593
Alasdair Tran <alasdair.tran@gmail.com>
2022-09-26 10:44:50 +0000

    Create dedicated zongyi_fno module

--- fourierflow/modules/__init__.py
+++ fourierflow/modules/__init__.py
@@ -1,12 +1,9 @@
 from .factorized_fno import (FNOFactorized2DBlock, FNOFactorizedMesh2D,
                              FNOFactorizedPointCloud2D,
                              FNOFullyFactorizedMesh2D)
-from .fno_mesh_2d import FNOMesh2D
-from .fno_mesh_3d import FNOMesh3D
 from .fno_plus_2d import FNOPlus2DBlock
-from .fno_point_cloud_2d import FNOPointCloud2D
-from .fno_zongyi_2d import FNOZongyi2DBlock
 from .iphi import IPhi
 from .linear import GehringLinear, WNLinear
 from .normalizer import Normalizer
 from .position import fourier_encode
+from .zongyi_fno import FNOMesh2D, FNOMesh3D, FNOPointCloud2D, FNOZongyi2DBlock
---
+++ fourierflow/modules/zongyi_fno/__init__.py
@@ -0,0 +1,4 @@
+from .grid_2d import FNOZongyi2DBlock
+from .mesh_2d import FNOMesh2D
+from .mesh_3d import FNOMesh3D
+from .point_cloud_2d import FNOPointCloud2D
