d28ab24c5703da3e960d38ff839f1cf54d511bdf
Alasdair Tran <alasdair.tran@gmail.com>
2022-09-26 10:47:06 +0000

    Rename new mesh implementation

--- fourierflow/modules/factorized_fno/__init__.py
+++ fourierflow/modules/factorized_fno/__init__.py
@@ -1,5 +1,5 @@
 from .grid_2d import FNOFactorized2DBlock
 from .grid_plus_2d import FNOPlus2DBlock
 from .mesh_2d import FNOFactorizedMesh2D
-from .mesh_2d_v2 import FNOFullyFactorizedMesh2D
+from .mesh_plus_2d import FNOFullyFactorizedMesh2D
 from .point_cloud_2d import FNOFactorizedPointCloud2D
