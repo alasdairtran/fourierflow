d89b1c908beefed7d21c4a68d49631f5a984dbb4
Alasdair Tran <alasdair.tran@gmail.com>
2022-09-26 10:56:01 +0000

    Distinguish between Jax and PyTorch MeshGraphNet

--- fourierflow/routines/__init__.py
+++ fourierflow/routines/__init__.py
@@ -1,6 +1,6 @@
 from .grid_2d_markov import Grid2DMarkovExperiment
 from .grid_2d_rollout import Grid2DRolloutExperiment
 from .learned_interpolator import LearnedInterpolator
-from .meshgraphnet import MeshGraphNet
+from .meshgraphnet_jax import MeshGraphNet
 from .point_cloud import PointCloudExperiment
 from .structured_mesh import StructuredMeshExperiment
