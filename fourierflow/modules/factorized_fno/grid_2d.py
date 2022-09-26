78b9812b4869ae40a625cd8916cafa41cd317a84
Alasdair Tran <alasdair.tran@gmail.com>
2022-09-26 12:19:32 +0000

    Reuse FeedForward module

--- fourierflow/modules/factorized_fno/grid_2d.py
+++ fourierflow/modules/factorized_fno/grid_2d.py
@@ -3,28 +3,8 @@ import torch
 import torch.nn as nn
 from einops import rearrange
 
+from ..feedforward import FeedForward
 from ..linear import WNLinear
-
-
-class FeedForward(nn.Module):
-    def __init__(self, dim, factor, ff_weight_norm, n_layers, layer_norm, dropout):
-        super().__init__()
-        self.layers = nn.ModuleList([])
-        for i in range(n_layers):
-            in_dim = dim if i == 0 else dim * factor
-            out_dim = dim if i == n_layers - 1 else dim * factor
-            self.layers.append(nn.Sequential(
-                WNLinear(in_dim, out_dim, wnorm=ff_weight_norm),
-                nn.Dropout(dropout),
-                nn.ReLU(inplace=True) if i < n_layers - 1 else nn.Identity(),
-                nn.LayerNorm(out_dim) if layer_norm and i == n_layers -
-                1 else nn.Identity(),
-            ))
-
-    def forward(self, x):
-        for layer in self.layers:
-            x = layer(x)
-        return x
 
 
 class SpectralConv2d(nn.Module):
