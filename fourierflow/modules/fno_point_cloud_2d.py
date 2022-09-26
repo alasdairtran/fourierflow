b2abf6f9ed6429a1dfa76f8dc4048bbb5b76b3e9
Alasdair Tran <alasdair.tran@gmail.com>
2022-09-26 10:15:57 +0000

    Rename FNOMesh2D to FNOPointCloud2D

--- fourierflow/modules/fno_point_cloud_2d.py
+++ fourierflow/modules/fno_point_cloud_2d.py
@@ -152,7 +152,7 @@ class SpectralConv2d(nn.Module):
         return Y
 
 
-class FNOMesh2D(nn.Module):
+class FNOPointCloud2D(nn.Module):
     def __init__(self, modes1, modes2, width, in_channels, out_channels,
                  n_layers=4, is_mesh=True, s1=40, s2=40):
         super().__init__()
